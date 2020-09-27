ee4e8c97cece37e01c6082ac8a0ef6c34429c429
GustavoMSevero <gustavo.msevero@gmail.com>
2020-09-27 15:31:22 -0300

    Saying time and date

--- assistant.py
+++ assistant.py
@@ -1,4 +1,5 @@
 import pyttsx3 # pip install pyttxs3
+import datetime
 
 engine = pyttsx3.init()
 
@@ -6,4 +7,19 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
-speak("Hi! i'm JARVIS, an virtual assistant.")
+def timeNow():
+    time = datetime.datetime.now().strftime("%I:%M")
+    speak(time)
+
+def dateToday():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    day = int(datetime.datetime.now().day)
+    speak(day)
+    speak(month)
+    speak(year)
+
+
+# speak("Hi! i'm JARVIS, an virtual assistant.")
+timeNow()
+dateToday()
